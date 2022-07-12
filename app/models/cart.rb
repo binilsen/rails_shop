affbed45693168cb57397c0e348b7fe748534c45
binilsen <binil.senb@techversantinfo.com>
2022-07-12 18:08:01 +0530

    fix bootstrap style overlap form errors

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   has_many :carts_products, dependent: :destroy
   has_many :products, :through => :carts_products
   has_one :order, dependent: :destroy
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
-  has_many :carts
-  has_many :orders
+  has_many :carts,dependent: :destroy
+  has_many :orders,dependent: :destroy
 end
