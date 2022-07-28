06296881ae4baa206cc727008022ad0b25b6a360
binilsen <binil.senb@techversantinfo.com>
2022-07-28 12:43:42 +0530

    fix bootstrap js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
-# frozen_string_literal: true
-
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', preload: true
+pin 'bootstrap', to: 'bootstrap.js'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
